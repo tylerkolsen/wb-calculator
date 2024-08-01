97d713818d62fac9b49bcdb49a1cc0532e66ed78
Tyler Olsen <tylerkolsen@gmail.com>
2024-08-01 14:01:21 -0600

    finished writing math functions

--- calculator.js
+++ calculator.js
@@ -1,3 +1,38 @@
+// Add function
+function add(num1, num2) {
+  return num1 + num2
+}
+
+// Subtract function
+function subtract(num1, num2) {
+  return num1 - num2
+}
+
+// Multiply function
+function multiply(num1, num2) {
+  return num1 * num2
+}
+
+// Divide function
+function divide(num1, num2) {
+  return num1 / num2
+}
+
+// Power function
+function power(num1, num2) {
+  return num1 ** num2
+}
+
+// Remainder function
+function remainder(num1, num2) {
+  return num1 % num2
+}
+
+// Square Root function
+function sqrt(num1) {
+  return Math.sqrt(num1)
+}
+
 function calculate(expression) {}
 
 /* **************** DO NOT EDIT THE CODE BELOW **************** */
